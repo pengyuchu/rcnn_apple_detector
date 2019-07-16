f36e54791fe8f77a69108e3d6a8a562803108a8d
movinggarrett12 <thornsb4@msu.edu>
2019-07-16 13:57:58 -0400

    Setup.bash changes for system

--- setup.bash
+++ setup.bash
@@ -1,7 +1,7 @@
 # Run it before rosrun
 
-CATKIN_WORKSPACE=/home/bizon/catkin_workspace
+CATKIN_WORKSPACE=/home/rival/Documents/Repos/ROS
 
 source $CATKIN_WORKSPACE/devel/setup.bash
-export CMAKE_PREFIX_PATH=$CATKIN_WORKSPACE/devel:/opt/ros/melodic
-export ROS_PACKAGE_PATH=$CATKIN_WORKSPACE/src:/opt/ros/melodic/share
+export CMAKE_PREFIX_PATH=$CATKIN_WORKSPACE/devel:/opt/ros/kinetic
+export ROS_PACKAGE_PATH=$CATKIN_WORKSPACE/src:/opt/ros/kinetic/share
